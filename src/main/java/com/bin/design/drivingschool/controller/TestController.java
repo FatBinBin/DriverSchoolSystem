7f858049c3d1e32827b4b84b08facc4c21bd965d
Bin <1821613971@qq.com>
2018-10-21 09:27:12 +0800

    环境初始化

---
+++ src/main/java/com/bin/design/drivingschool/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.bin.design.drivingschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @RequestMapping(value = "begin")
+    public String test(){
+        return "helloWorld";
+    }
+}
