c98dfc12594d7d92f8427b7679f9b807fd6eee18
fatbinbin <1821613971@qq.com>
2018-10-21 09:47:20 +0800

    环境初始化

--- src/main/java/com/bin/design/drivingschool/controller/TestController.java
+++ src/main/java/com/bin/design/drivingschool/controller/TestController.java
@@ -1,8 +1,8 @@
 package com.bin.design.drivingschool.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
